48f9a5c3a7c158c8e619cb84760ebcf4f9da26dc
headshot2017 <>
2025-07-17 02:43:45 -0400

    GUI tool: show hitbox in preview

--- gui-tool/animatorgraphicsview.py
+++ gui-tool/animatorgraphicsview.py
@@ -13,6 +13,13 @@ def convertPosToUnity(posList):
 def convertScaleToUnity(scale):
     return scale/0.4
 
+def convertHitboxPosToUnity(posList):
+    return [posList[0]*50, posList[1]*50]
+
+def calculateHitboxCoordinates(pos, scale):
+    pos2 = [pos[0] - 0.5*scale[0], pos[1] + 0.5*scale[1]]
+    return convertPosToUnity(pos2), convertHitboxPosToUnity(scale)
+
 
 class PixmapAnimator(QtCore.QAbstractAnimation):
     def __init__(self, graphicsView):
@@ -20,7 +27,13 @@ class PixmapAnimator(QtCore.QAbstractAnimation):
         self.graphicsView = graphicsView
 
         self.pixmapItem = QtWidgets.QGraphicsPixmapItem()
+        self.hitboxItem = QtWidgets.QGraphicsRectItem(0, 0, 0, 0)
         self.graphicsView.scene().addItem(self.pixmapItem)
+        self.graphicsView.scene().addItem(self.hitboxItem)
+        self.hitboxItem.setPos(self.graphicsView.sceneRect().width()/2, self.graphicsView.sceneRect().height()/2)
+
+        self.hitboxItem.setBrush(QtGui.QColor(255, 0, 0, 128))
+        self.hitboxItem.setPen(QtGui.QColor(255, 0, 0, 255))
 
         self.fullPixmap = None
         self.animDict = None
@@ -38,6 +51,7 @@ class PixmapAnimator(QtCore.QAbstractAnimation):
         path = gamepath.getCharacterPath(characterdata.name)
         self.fullPixmap = QtGui.QPixmap("%s/sheet.png" % path)
         self.setSprite(0,0,0,0)
+        self.hitboxItem.setRect(0, 0, 0, 0)
 
     def setAnimation(self, anim):
         self.stop()
@@ -81,8 +95,7 @@ class PixmapAnimator(QtCore.QAbstractAnimation):
 
         globalScale = self.animDict["scale"] if "scale" in self.animDict else [1, 1]
         globalCharScale = convertScaleToUnity(characterdata.jsonFile["general"]["scale"]["ingame"])
-        globalScale[0] *= globalCharScale
-        globalScale[1] *= globalCharScale
+        globalScale = [globalScale[0] * globalCharScale, globalScale[1] * globalCharScale]
 
         frame = self.animDict["frames"][self.frame]
 
@@ -99,23 +112,46 @@ class PixmapAnimator(QtCore.QAbstractAnimation):
             globalScale[1] * (frame["scale"][1] if "scale" in frame else 1)
         ))
 
-        if "frame" in frame:
-            self.setSprite(*frame["frame"])
         if playSound and "sound" in frame and frame["sound"] in characterdata.sounds:
             characterdata.sounds[frame["sound"]].play()
+
+        spriteFrame = frame
+        spriteFrameInd = self.frame
+        while "frame" not in spriteFrame and spriteFrameInd > 0:
+            spriteFrameInd -= 1
+            spriteFrame = self.animDict["frames"][spriteFrameInd]
+        if "frame" in spriteFrame:
+            self.setSprite(*spriteFrame["frame"])
+
+        hitboxFrame = frame
+        hitboxFrameInd = self.frame
+        while "hitbox" not in hitboxFrame and hitboxFrameInd > 0:
+            hitboxFrameInd -= 1
+            hitboxFrame = self.animDict["frames"][hitboxFrameInd]
+        if "hitbox" in hitboxFrame:
+            hitbox = hitboxFrame["hitbox"]
+            on = hitbox["on"]
+            pos, scale = calculateHitboxCoordinates(
+                hitbox["pos"] if "pos" in hitbox else [0, 0],
+                hitbox["scale"] if "scale" in hitbox else [0, 0],
+            )
+
+            self.hitboxItem.setRect(pos[0], pos[1], scale[0], scale[1])
+            self.hitboxItem.setBrush(QtGui.QColor(255, 0, 0, 0 if not on else 128))
+            self.hitboxItem.setVisible(True)
+        else:
+            self.hitboxItem.setVisible(False)
 
         return True
 
     def interpolateFrames(self, x):
         globalOffset = self.animDict["offset"] if "offset" in self.animDict else [0, 0]
         globalCharOffset = characterdata.jsonFile["general"]["offset"]["ingame"]
-        globalOffset[0] += globalCharOffset[0]
-        globalOffset[1] += globalCharOffset[1]
+        globalOffset = [globalOffset[0] + globalCharOffset[0], globalOffset[1] + globalCharOffset[1]]
 
         globalScale = self.animDict["scale"] if "scale" in self.animDict else [1, 1]
         globalCharScale = convertScaleToUnity(characterdata.jsonFile["general"]["scale"]["ingame"])
-        globalScale[0] *= globalCharScale
-        globalScale[1] *= globalCharScale
+        globalScale = [globalScale[0] * globalCharScale, globalScale[1] * globalCharScale]
 
         currAction = self.animDict["frames"][self.frame]
         if currAction["delay"] <= 0: return
@@ -125,6 +161,48 @@ class PixmapAnimator(QtCore.QAbstractAnimation):
             nextAction = self.animDict["frames"][0] if self.loopCount() < 0 or self.currentLoop() < self.loopCount()-1 else currAction
         else:
             nextAction = self.animDict["frames"][self.frame+1]
+
+        if "hitbox" in currAction and "hitbox" in nextAction:
+            on = currAction["hitbox"]["on"]
+
+            nextHitbox = nextAction["hitbox"]
+            hitboxOff = (
+                not nextHitbox["on"] and
+                ("pos" not in nextHitbox or nextHitbox["pos"] == [0,0]) and
+                ("scale" not in nextHitbox or nextHitbox["scale"] == [0,0])
+            )
+            if hitboxOff:
+                nextHitbox = currAction["hitbox"]
+
+            posLerp = [
+                lerp(
+                    currAction["hitbox"]["pos"][0] if "pos" in currAction["hitbox"] else 0,
+                    nextHitbox["pos"][0] if "pos" in nextHitbox else 0,
+                    x
+                ),
+                lerp(
+                    currAction["hitbox"]["pos"][1] if "pos" in currAction["hitbox"] else 0,
+                    nextHitbox["pos"][1] if "pos" in nextHitbox else 0,
+                    x
+                )
+            ]
+            scaleLerp = [
+                lerp(
+                    currAction["hitbox"]["scale"][0] if "scale" in currAction["hitbox"] else 0,
+                    nextHitbox["scale"][0] if "scale" in nextHitbox else 0,
+                    x
+                ),
+                lerp(
+                    currAction["hitbox"]["scale"][1] if "scale" in currAction["hitbox"] else 0,
+                    nextHitbox["scale"][1] if "scale" in nextHitbox else 0,
+                    x
+                )
+            ]
+            posLerp, scaleLerp = calculateHitboxCoordinates(posLerp, scaleLerp)
+
+            self.hitboxItem.setRect(posLerp[0], posLerp[1], scaleLerp[0], scaleLerp[1])
+            self.hitboxItem.setBrush(QtGui.QColor(255, 0, 0, 0 if not on else 128))
+
 
         angleLerp = lerp(
             currAction["angle"] if "angle" in currAction else 0,
